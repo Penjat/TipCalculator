e5b79a098f5b980547a7a23b83bcbd2de4cca05c
Penjat <theSpencerSymington@Gmail.com>
2019-01-18 07:51:40 -0800

    calculates amount and total from percent and billAmount

--- TipCalculator/TipViewController.h
+++ TipCalculator/TipViewController.h
@@ -10,7 +10,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface TipViewController : UIViewController
+@interface TipViewController : UIViewController <UITextFieldDelegate>
 
 @end
 
--- TipCalculator/TipViewController.m
+++ TipCalculator/TipViewController.m
@@ -9,6 +9,11 @@
 #import "TipViewController.h"
 
 @interface TipViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billTotalField;
+@property (weak, nonatomic) IBOutlet UITextField *tipPercentField;
+@property (weak, nonatomic) IBOutlet UITextField *totalField;
+@property (weak, nonatomic) IBOutlet UITextField *tipAmountField;
+
 
 @end
 
@@ -17,8 +22,47 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    self.billTotalField.delegate = self;
+    self.tipPercentField.delegate = self;
+    
 }
-
+-(void)calculateValues:(long int)tag{
+    
+    //get all the values
+    float billAmount = [self.billTotalField.text floatValue];
+    float tippercent = [self.tipPercentField.text floatValue];
+    float tipAmount = [self.tipAmountField.text floatValue];
+    float totalAmount = [self.totalField.text floatValue];
+    
+    //calculate based on tip percent
+    if(tag == 0 || tag == 1){
+        
+        
+        tipAmount = (tippercent/100)*billAmount;
+        
+        self.tipAmountField.text = [NSString stringWithFormat:@"%.02f",tipAmount ];
+        
+        //getting value from text field to avoid rounding errors
+        self.totalField.text = [NSString stringWithFormat:@"%.02f",(billAmount + [self.tipAmountField.text floatValue]) ];
+    }else if (tag == 2){
+        //if a specific tip ampount was entered
+        //TODO check for 0.00 total
+        
+        
+    }
+    
+    
+    
+    
+    
+}
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    NSLog(@"enter was pressed");
+    //TODO use tag to tell which on was pressed
+    [self calculateValues:textField.tag];
+    
+    return YES;
+}
 /*
 #pragma mark - Navigation
 
@@ -30,3 +74,5 @@
 */
 
 @end
+
+
